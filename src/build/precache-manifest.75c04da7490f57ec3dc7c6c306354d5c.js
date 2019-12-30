b6b160fd5ccf882b9833c88f1db7f390c2573146
Ravinder <rnath@landolakes.com>
2019-12-30 12:03:00 -0600

    This is 1st get exposure

---
+++ src/build/precache-manifest.75c04da7490f57ec3dc7c6c306354d5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f83f61c59936c2ac520",
+    "url": "/static/css/main.07ec77ca.chunk.css"
+  },
+  {
+    "revision": "1f83f61c59936c2ac520",
+    "url": "/static/js/main.773ce52e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1410f67aa89e49b2ba06",
+    "url": "/static/js/2.a5ce7fec.chunk.js"
+  },
+  {
+    "revision": "44b383befcea3aa652012d2511505597",
+    "url": "/index.html"
+  }
+];
